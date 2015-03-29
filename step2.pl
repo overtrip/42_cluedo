86f2c4f58345b93e92a091d63b2fac3e8c393c37
Jeremy ALONSO <jealonso@e3r9p14.42.fr>
2015-03-29 22:22:28 +0200

    step2 changed

--- step2.pl
+++ step2.pl
@@ -1,8 +1,8 @@
-maisons(0, []).
-maison(N, [(_Nationalite,_Animal,_Couleur,_Boisson,_Fume)|T]):- N1 is N-1 , maisons(N1,T).
+maisons(0,[]).
+maisons(N,[(_Nationalite,_Animal,_Couleur,_Boisson,_Fume)|T]):- N1 is N-1 , maisons(N1,T).
 
-maison(1, [H|_], H).
-maison(N, [_|T], R) :- N1 is N-1 , maison(N1, T, R).
+maison(1,[H|_], H).
+maison(N,[_|T], R) :- N1 is N-1 , maison(N1, T, R).
 
 indice1([(anglais,_,rouge,_,_)|_]).
 indice1([_|T]):- indice1(T).
@@ -26,11 +26,11 @@ indice7([(_,cheval,_,_,_), (_,_,jaune,_,dunhill)|_]).
 indice7([(_,_,jaune,_,dunhill), (_,cheval,_,_,_)|_]).
 indice7([_|T]):- indice7(T).
 
-indice8(maisons) :- maison(1, Maisons, (norvegien,_,_,_,_)).
+indice8(Maisons) :- maison(1,Maisons,(norvegien,_,_,_,_)).
 
-indice9(maisons) :- maison(2, Maisons, (_,_,bleu,_,_)).
+indice9(Maisons) :- maison(2,Maisons,(_,_,bleu,_,_)).
 
-indice10(maisons) :- maison(3, Maisons, (_,_,_,lait,_)).
+indice10(Maisons) :- maison(3,Maisons,(_,_,_,lait,_)).
 
 indice11([(_,_,_,_,blend), (_,chat,_,_,_)|_]).
 indice11([(_,chat,_,_,_), (_,_,_,_,blend)|_]).
@@ -46,10 +46,7 @@ indice13([_|T]):- indice13(T).
 indice14([(allemand,_,_,_,prince)|_]).
 indice14([_|T]):- indice14(T).
 
-indice15([(_,_,_,eau,_)|_]).
-indice15([_|T]):- indice15(T).
-
 question([(_,poisson,_,_,_)|_]).
-question([_|T]).
+question([_|T]):- question(T).
 
-solution(Maisons) :- maisons(5, Maisons), indice1(Maisons), indice2(Maisons), indice3(Maisons), indice4(Maisons), indice5(Maisons), indice6(Maisons), indice7(Maisons), indice8(Maisons), indice8(Maisons), indice9(Maisons), indice10(Maisons), indice11(Maisons), indice12(Maisons), indice13(Maisons), indice14(Maisons), indice15(Maisons), question(Maisons).
+solution(Maisons) :- maisons(5, Maisons), indice1(Maisons), indice2(Maisons), indice3(Maisons), indice4(Maisons), indice5(Maisons), indice6(Maisons), indice7(Maisons), indice8(Maisons), indice8(Maisons), indice9(Maisons), indice10(Maisons), indice11(Maisons), indice12(Maisons), indice13(Maisons), indice14(Maisons), question(Maisons).
